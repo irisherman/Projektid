95e2eb76ca28f0d982dde65c5d6ed46fef2d72a8
irisherman <69869027+irisherman@users.noreply.github.com>
2022-02-02 18:58:55 +0200

    loeng08

---
+++ loeng08/kodunetöö.js
@@ -0,0 +1,15 @@
+let kasutajaNimi= prompt()
+let tervitus = "Tere " + kasutajaNimi
+let tervitusElement = document.getElementById("tervitus")
+
+if (kasutajaNimi.length ==0){
+    tervitus = "Sul jäi nimi sisestamata"
+}
+
+
+tervitusElement.innerText = tervitus
+console.log(tervitus)
+
+for (let index = 0; index <=6 kasutajaNimi.length;) {
+    tervitus ='Küll sul on ilus lühike nimi')
+} 
