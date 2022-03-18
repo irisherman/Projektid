54341d6767c2f4c984692d2cae97be0c63ac9877
irisherman <69869027+irisherman@users.noreply.github.com>
2022-03-18 23:16:06 +0200

    m

--- loeng11/indeks.js
+++ loeng11/indeks.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+
+function tervitus2(req, res) {
+    res.end('<h1>Tere k6ik karvased ja sulelised </h1>')
+}
 app.get('/', function(req, res){
     res.end('Tere maailm')
 })
+
+app.get('/tervitus2', tervitus2)
+
 app.listen(3000)
